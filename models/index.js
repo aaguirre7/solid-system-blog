84addf769d139bf83c936306838a9178a2714e75
Abraham Aguirre <aguirre.abraham@gmail.com>
2022-05-20 13:15:17 -0400

    completed modules

---
+++ models/index.js
@@ -0,0 +1,35 @@
+//import modules
+const User = require('./User');
+const Post = require('./Post');
+const Comment = require('./Comment');
+//start associations
+User.hasMany(Post, {
+    foreignKey: 'user_id'
+});
+Post.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: "cascade"
+});
+
+Comment.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: "cascade"
+});
+
+Comment.belongsTo(Post, {
+    foreignKey: 'post_id',
+    onDelete: "cascade"
+});
+
+User.hasMany(Comment, {
+    foreignKey: 'user_id',
+    onDelete: "cascade"
+});
+
+Post.hasMany(Comment, {
+    foreignKey: 'post_id',
+    onDelete: "cascade"
+})
+
+//export modules
+module.exports = { User, Post, Comment };
